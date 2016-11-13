9459786f01cbbf4f9f8b1cb41f8dca81ba325c32
abrahn <abrahn@uw.edu>
2016-11-13 15:24:05 -0800

    Class 3 Polynomial Exercise

---
+++ Class Exercises/Class3Polynomial/Class3Polynomial/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+    class Program
+    {
+        static void Main()
+        {
+ 
+        Console.Write("Please choose a number for x: "); //prompting user for input
+        string strx = Console.ReadLine(); //reading and storing the input from user
+        int x = int.Parse(strx); //converting the string x to an integer
+
+        int y = (int)(3*(x * x* x) - 5*(x*x) +6); //Here is the polynomial to be calculated
+
+        Console.WriteLine("Here is the answer: " + y);
+        Console.ReadLine(); //This keeps the console open
+
+
+        }
+    }
+
