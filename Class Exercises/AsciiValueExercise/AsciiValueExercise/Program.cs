9321f9850af2510da36340e413e43e156d4f9518
abrahn <abrahn@uw.edu>
2016-11-13 17:43:15 -0800

    Class 4 Exercise

---
+++ Class Exercises/AsciiValueExercise/AsciiValueExercise/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+    class AsciiValue
+    {
+        static void Main()
+        {
+        Console.Write("Fine Ascii value for: ");  //  ask user for number
+        string strx = Console.ReadLine();        // read and store input
+        char c = strx[0];        // only take first character of input
+        int asciiValue = (int)c;                         // convert to Ascii value
+
+        // show user value in console
+        Console.WriteLine("Ascii ={0}", asciiValue);
+
+
+
+
+        Console.ReadLine();
+        }
+    }
