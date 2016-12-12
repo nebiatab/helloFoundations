7258699f8b8172e4b1e325370797c12d4b609a03
abrahn <abrahn@uw.edu>
2016-12-12 02:26:39 -0800

    Hw 6 complete!

---
+++ Class Exercises/Struct/Struct/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+// File: Struct.cs
+using System;
+
+struct Person
+{
+    public string Name;
+    public int Age;
+}
+
+class Test
+{
+    static void Main()
+    {
+        Person[] persons = new Person[2];
+
+        persons[0].Name = "Steve";
+        persons[0].Age = 31;
+
+        persons[1].Name = "Lisa";
+        persons[1].Age = 28;
+
+        foreach (Person p in persons)
+        {
+            Console.WriteLine("{0} is {1} years old.", p.Name, p.Age);
+        }
+    }
+}
